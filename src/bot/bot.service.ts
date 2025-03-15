505072e27e14b7a8bf305f4ee3c4741944d696cf
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2025-03-15 00:41:32 -0300

    return to prisma

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -2,28 +2,17 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { Client, LocalAuth } from 'whatsapp-web.js';
 import { PrismaClient } from '@prisma/client';
-const chromium = require('chrome-aws-lambda');
 import * as fs from 'fs';
 import * as path from 'path';
 import { promisify } from 'util';
 
 const rm = promisify(fs.rm);
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-
-
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 @Injectable()
 export class BotService implements OnModuleInit {
   private client: Client = new Client({
-    authStrategy: new LocalAuth({
-      dataPath: '/tmp/.wwebjs_auth',
-    }),
-    puppeteer: {
-      executablePath: chromium.executablePath, // Use the pre-installed Chromium
-        args: chromium.args, // Additional Chromium args
-        headless: true // Run in headless mode
-    },
+    authStrategy: new LocalAuth(),
   });
   private enterpriseId: string = '';
   private prisma = new PrismaClient();
@@ -32,23 +21,7 @@ export class BotService implements OnModuleInit {
 
   constructor(private eventEmitter: EventEmitter2) {}
 
-  async onModuleInit() {
-    const sessionPath = path.join('/tmp', '.wwebjs_auth', 'session');
-    const parentPath = path.dirname(sessionPath);
-
-    // Create the parent directory if it doesn't exist
-    if (!fs.existsSync(parentPath)) {
-      fs.mkdirSync(parentPath, { recursive: true });
-    }
-
-    const cachePath = path.join('/tmp', '.wwebjs_cache');
-    const cacheParentPath = path.dirname(cachePath);
-
-    // Create the parent directory if it doesn't exist
-    if (!fs.existsSync(cacheParentPath)) {
-      fs.mkdirSync(cacheParentPath, { recursive: true });
-    }
-
+  onModuleInit() {
     this.client.on('qr', (qr) => {
       this.logger.log(
         `QrCode: http://localhost:${3000}/bot/qrcode/421a24c1-e02d-44b3-af45-267e1b5e306c`,
@@ -66,83 +39,64 @@ export class BotService implements OnModuleInit {
 
     this.client.on('message', async (msg) => {
       this.logger.verbose(`${msg.from}: ${msg.body}`);
-      const messages = await this.prisma.messages.findMany({
-        where: { enterpriseId: this.enterpriseId },
-        orderBy: { numOrder: 'asc' },
-      });
+      const messages = await this.prisma.messages.findMany({where: {enterpriseId: this.enterpriseId}, orderBy: {numOrder: 'asc'}});
       let m;
-      if (msg.body === '1') {
+      if(msg.body === '1') {
         advance = 1;
-        numOrderCount = numOrderCount + advance;
-      } else if (msg.body === '2') {
+        numOrderCount = numOrderCount+advance;
+      } else if(msg.body === '2') {
         advance = 2;
-        numOrderCount = numOrderCount + advance;
-      } else if (msg.body === '3') {
+        numOrderCount = numOrderCount+advance;
+      } else if(msg.body === '3') {
         advance = 3;
-        numOrderCount = numOrderCount + advance;
+        numOrderCount = numOrderCount+advance;
       }
 
-      for (let i = numOrderCount; i <= messages.length; ) {
-        if (
-          messages[i]?.parentMessageId === null &&
-          messages[i]?.option === 'MENU'
-        ) {
+      for(let i = numOrderCount; i <= messages.length;) {
+
+        if(messages[i]?.parentMessageId === null && messages[i]?.option === 'MENU') {
           m = messages[i]?.body;
           msg.reply(m);
           idPadre = messages[i]?.id;
           break;
         }
 
-        if (
-          messages[i]?.parentMessageId === idPadre &&
-          messages[i]?.option === 'MENU' &&
-          messages[i]?.trigger?.toLowerCase() === msg.body.toLowerCase()
-        ) {
-          const counti = await this.prisma.messages.count({
-            where: { parentMessageId: messages[i]?.id },
-          });
-          const x = await this.prisma.messages.findUnique({
-            where: { id: idPadre },
-          });
+        if(messages[i]?.parentMessageId === idPadre && messages[i]?.option === 'MENU' && messages[i]?.trigger?.toLowerCase() === msg.body.toLowerCase()) {
+          const counti = await this.prisma.messages.count({where: {parentMessageId: messages[i]?.id}});
+          const x = await this.prisma.messages.findUnique({where: {id: idPadre}});
           m = messages[i]?.body;
           msg.reply(m);
           idPadre = messages[i]?.id;
           break;
         }
 
-        if (
-          messages[i]?.parentMessageId === idPadre &&
-          messages[i]?.trigger?.toLowerCase() === msg.body.toLowerCase()
-        ) {
-          const counti = await this.prisma.messages.count({
-            where: { parentMessageId: messages[i]?.id },
-          });
-          const x = await this.prisma.messages.findUnique({
-            where: { id: idPadre },
-          });
+        if(messages[i]?.parentMessageId === idPadre && messages[i]?.trigger?.toLowerCase() === msg.body.toLowerCase()) {
+          const counti = await this.prisma.messages.count({where: {parentMessageId: messages[i]?.id}});
+          const x = await this.prisma.messages.findUnique({where: {id: idPadre}});
           m = messages[i]?.body;
           msg.reply(m);
           idPadre = messages[i]?.id;
           //numOrderCount = 0;
-          if (messages[i]?.finishLane === true) {
+          if(messages[i]?.finishLane === true) {
             numOrderCount = 0;
             break;
           } else {
             break;
           }
+
         }
 
-        if (i == messages.length - 1) {
+        if(i == messages.length-1) {
           m = messages[i]?.body;
           await msg.reply(m);
           numOrderCount = 0;
           break;
         }
 
-        if (numOrderCount === 0) {
+        if(numOrderCount === 0) {
           m = messages[i]?.body;
           await msg.reply(m);
-          numOrderCount = numOrderCount + 1;
+          numOrderCount = numOrderCount+1;
         }
         i++;
       }
